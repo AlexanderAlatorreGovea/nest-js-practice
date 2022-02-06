bfeb6c5158f5788c600a6e517eec22c0d04c08d7
Alexander <Alexander_alatorre1993@hotmail.com>
2022-02-05 17:56:55 -0800

    Added posts

--- src/post/dto/create-post.dto.ts
+++ src/post/dto/create-post.dto.ts
@@ -1,1 +1,15 @@
-export class CreatePostDto {}
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class CreatePostDto {
+  @IsString({ each: true })
+  @IsNotEmpty()
+  paragraphs: string[];
+
+  @IsString()
+  @IsNotEmpty()
+  title: string;
+
+  @IsString()
+  id: number;
+
+}
--- src/post/dto/update-post.dto.ts
+++ src/post/dto/update-post.dto.ts
@@ -1,4 +1,9 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { IsNumber, IsOptional } from 'class-validator';
 import { CreatePostDto } from './create-post.dto';
 
-export class UpdatePostDto extends PartialType(CreatePostDto) {}
+export class UpdatePostDto extends CreatePostDto {
+  @IsNumber()
+  @IsOptional()
+  id: number;
+}
